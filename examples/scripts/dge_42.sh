60e1daf998e1fc11dce96b1b88bef120a21c1f00
acristo <acristo@broadinstitute.org>
2019-05-01 17:00:18 -0700

    Added complete example

---
+++ examples/scripts/dge_42.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+source /broad/software/scripts/useuse
+use R-3.5
+
+WD="/btl/foundry/users/alex/20190228_novel_chassis/run_DGE/"
+RESULTS=$JOB_ID
+RUNDIR="$WD$RESULTS"
+mkdir $RUNDIR
+cd $RUNDIR
+
+Rscript $WD/scripts/dge_42.r > $WD/$RESULTS/dge_42_log.txt
