7322403be1a2df506ca28789fdfcb052cda0c26a
urrutia <root@js-170-248.jetstream-cloud.org>
2019-08-08 10:29:02 -0400

    scaling up the test script

--- tests/test.py
+++ tests/test.py
@@ -20,14 +20,14 @@ for x in ['Temp', 'Timepoint']:
 
 sub_factors = ['Timepoint', 'Temp', 'Arabinose', 'IPTG']
 DE_tests = [
-    #['Strain1MG1655WT', 'Strain1MG1655WT']]#,
-    #['Strain1MG1655WT', 'Strain2MG1655GenomicPhlFGate'], #WT vs all
-    #['Strain1MG1655WT', 'Strain3MG1655GenomicIcaRGate'],
-    #['Strain1MG1655WT', 'Strain4MG1655GenomicNANDCircuit'],
-    ['Strain4MG1655GenomicNANDCircuit', 'Strain3MG1655GenomicIcaRGate']] #, #NAND vs IcaR genome
-    #['Strain4MG1655GenomicNANDCircuit', 'Strain2MG1655GenomicPhlFGate'], #NAND vs PhlF genome
-    #['Strain4MG1655GenomicNANDCircuit', 'Strain4MG1655GenomicNANDCircuit']  #NAND vs NAND
-#]
+    ['Strain1MG1655WT', 'Strain1MG1655WT'],
+    ['Strain1MG1655WT', 'Strain2MG1655GenomicPhlFGate'], #WT vs all
+    ['Strain1MG1655WT', 'Strain3MG1655GenomicIcaRGate'],
+    ['Strain1MG1655WT', 'Strain4MG1655GenomicNANDCircuit'],
+    ['Strain4MG1655GenomicNANDCircuit', 'Strain3MG1655GenomicIcaRGate'], #NAND vs IcaR genome
+    ['Strain4MG1655GenomicNANDCircuit', 'Strain2MG1655GenomicPhlFGate'], #NAND vs PhlF genome
+    ['Strain4MG1655GenomicNANDCircuit', 'Strain4MG1655GenomicNANDCircuit']  #NAND vs NAND
+]
 
 groups_array = utils.group_by_factors(ginkgo_df, ['strain']+sub_factors)
 comparison_indices = comparison_generator.generate_comparisons(ginkgo_df, DE_tests, ['strain'], sub_factors, 1)
@@ -39,7 +39,7 @@ r_cmds = differential_expression.make_DE_cmds(
             sub_factors = sub_factors)
 print('Created {0} differential tests'.format(len(r_cmds)))
 
-deg_results = differential_expression.run_edgeR(r_cmds, cores=4)
+deg_results = differential_expression.run_edgeR(r_cmds, cores=8)
 
 KEGG_df = annotate_GO_KEGG.run_kegg(deg_results)
 with open('kegg_results.pkl', 'wb') as f:
