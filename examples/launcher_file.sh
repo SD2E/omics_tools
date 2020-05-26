2e24e48c09b77d71fa17aefe4dde81b22fab4450
Joshua Urrutia <urrutia@login1.wrangler.tacc.utexas.edu>
2020-05-26 13:22:42 -0500

    adding launcher example

---
+++ examples/launcher_file.sh
@@ -0,0 +1,14 @@
+singularity exec $STOCKYARD/singularity_cache/omics_tools-0.1.3.simg Rscript dge_1.r > results/dge_1_log.txt
+singularity exec $STOCKYARD/singularity_cache/omics_tools-0.1.3.simg Rscript dge_2.r > results/dge_2_log.txt
+singularity exec $STOCKYARD/singularity_cache/omics_tools-0.1.3.simg Rscript dge_3.r > results/dge_3_log.txt
+singularity exec $STOCKYARD/singularity_cache/omics_tools-0.1.3.simg Rscript dge_4.r > results/dge_4_log.txt
+singularity exec $STOCKYARD/singularity_cache/omics_tools-0.1.3.simg Rscript dge_5.r > results/dge_5_log.txt
+singularity exec $STOCKYARD/singularity_cache/omics_tools-0.1.3.simg Rscript dge_6.r > results/dge_6_log.txt
+singularity exec $STOCKYARD/singularity_cache/omics_tools-0.1.3.simg Rscript dge_7.r > results/dge_7_log.txt
+singularity exec $STOCKYARD/singularity_cache/omics_tools-0.1.3.simg Rscript dge_8.r > results/dge_8_log.txt
+singularity exec $STOCKYARD/singularity_cache/omics_tools-0.1.3.simg Rscript dge_9.r > results/dge_9_log.txt
+singularity exec $STOCKYARD/singularity_cache/omics_tools-0.1.3.simg Rscript dge_10.r > results/dge_10_log.txt
+singularity exec $STOCKYARD/singularity_cache/omics_tools-0.1.3.simg Rscript dge_11.r > results/dge_11_log.txt
+singularity exec $STOCKYARD/singularity_cache/omics_tools-0.1.3.simg Rscript dge_12.r > results/dge_12_log.txt
+singularity exec $STOCKYARD/singularity_cache/omics_tools-0.1.3.simg Rscript dge_13.r > results/dge_13_log.txt
+singularity exec $STOCKYARD/singularity_cache/omics_tools-0.1.3.simg Rscript dge_14.r > results/dge_14_log.txt
