e71cc26708caa0ba07f0415a79866e38890f62f5
gzheng2 <gzheng@eagc.test>
2020-07-03 10:54:29 -0400

    updated qc_update to use more generic filtering, simplified create_additive_design per Mohammed, added test for 23299

--- run_omics.py
+++ run_omics.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+import re
 import os
 import subprocess
 import time
@@ -9,8 +10,10 @@ from collections import Counter
 def qc_update(df, factors_to_keep, bool_factors, int_factors):
     df = df[ (df['QC_gcorr_BOOL']==True) & (df['QC_nmap_BOOL']==True) ]
 
+    patterns_to_filter = ["qc_", "_unit", "_input_state"]
+    columns_to_filter = ["replicate", "sample_id", "temperature", "timepoint"]
     for col in df.columns:
-        if col not in factors_to_keep and ("QC_" in col or "sample_id" in col or "Timepoint" in col):
+        if col not in factors_to_keep and (any(p in col.lower() for p in patterns_to_filter) or col.lower() in columns_to_filter):
             df.drop(col, axis=1, inplace=True)
     
     strain_column = ""
@@ -23,40 +26,79 @@ def qc_update(df, factors_to_keep, bool_factors, int_factors):
     return df
 
 def create_additive_design(df,int_cols=['IPTG','arabinose']):
-    genes = set(df.index)
-    genes_index = pd.DataFrame(list(range(len(genes))),columns=['Num_Index'],index=genes)
-    df = df.join(genes_index)
+    #genes = set(df.index)
+    #genes_index = pd.DataFrame(list(range(len(genes))),columns=['Num_Index'],index=genes)
+    #df = df.join(genes_index)
     for col in df.columns:
         if col in int_cols:
             df[col]=df[col].astype(int)
     df = pd.get_dummies(df,columns=['Timepoint'])
-    df_test = pd.get_dummies(df,columns=['Num_Index'])
-    return df_test
-
-def get_column(columns,part,property='FDR'):
-    #################
-    ###  moved this to wrangling.py and updated there
-    #################
-    '''
-    get the column name with an associated property
-    :param property: property output from omics-tools (logFC or FDR or nlogFDR)
-    :return: column name the column name associated with that (first one)
-    '''
-    col_name = [x for x in columns if((property in x) and (part in x))]
-    try:
-        col_name = col_name[0]
-        return col_name
-    except:
-        print("Ignoring part",part,"because part was not found")
-        return 0
+    #df_test = pd.get_dummies(df,columns=['Num_Index'])
+    #return df_test
+    return df
     
 def main(counts_df_path):
+    if "29422" in counts_df_path:
+        nand20 = False
+    else:
+        nand20 = True
     counts_df = pd.read_csv(counts_df_path, sep=',', low_memory=False)
     counts_df.rename({counts_df.columns[0]:'sample_id'},inplace=True,axis=1)
 
     print(counts_df.shape)
     base_factor = ['Strain']
-    bool_factors = ['IPTG', 'Cuminic_acid', 'Vanillic_acid', 'Xylose']
+
+    if nand20:
+        # for 23299
+        bool_factors=['IPTG','Arabinose']
+        local_test = True
+        if local_test:
+            DE_tests = [
+                ['MG1655','MG1655_LPV3'],
+                ['MG1655','MG1655_LPV3_AraC_Sensor'],
+                ['MG1655','MG1655_LPV3_AraC_Sensor_pBADmin_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pBADmin_PhlF'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pBADmin_PhlF_pPhlF_YFP']
+            ]
+        else:
+            DE_tests = [
+                ['MG1655','MG1655_LPV3'],
+                ['MG1655','MG1655_LPV3_AraC_Sensor'],
+                ['MG1655','MG1655_LPV3_AraC_Sensor_pBADmin_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pBADmin_PhlF'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pBADmin_PhlF_pPhlF_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pBADmin_PhlF_pTac_AmeR'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pBADmin_PhlF_pTac_AmeR_pPhlF_pAmeR_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pBADmin_PhlF_pTac_BM3R1'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pBADmin_PhlF_pTac_BM3R1_pPhlF_pBM3R1_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pBADmin_PsrA'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pBADmin_PsrA_pPsrA_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pBADmin_PsrA_pTac_AmeR'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pBADmin_PsrA_pTac_AmeR_pPsrA_pAmeR_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pBADmin_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pTac_AmeR'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pTac_AmeR_pAmeR_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pTac_BM3R1'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pTac_BM3R1_pBM3R1_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_AraC_Sensors_pTac_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_Sensor'],
+                ['MG1655','MG1655_LPV3_LacI_Sensor_pTac_AmeR'],
+                ['MG1655','MG1655_LPV3_LacI_Sensor_pTac_AmeR_pAmeR_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_Sensor_pTac_BM3R1'],
+                ['MG1655','MG1655_LPV3_LacI_Sensor_pTac_BM3R1_pBM3R1_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_Sensor_pTac_PhlF'],
+                ['MG1655','MG1655_LPV3_LacI_Sensor_pTac_PhlF_pPhlF_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_Sensor_pTac_PsrA'],
+                ['MG1655','MG1655_LPV3_LacI_Sensor_pTac_PsrA_pPsrA_YFP'],
+                ['MG1655','MG1655_LPV3_LacI_Sensor_pTac_YFP']
+            ]
+    else:
+        # for 29422
+        bool_factors = ['IPTG', 'Cuminic_acid', 'Vanillic_acid', 'Xylose']
+        DE_tests = [['Bacillus subtilis 168 Marburg', 'Bacillus subtilis 168 Marburg']]
+            
     int_factors = ['Timepoint']
     sub_factors = bool_factors + int_factors
     factors_to_keep = base_factor + sub_factors
@@ -64,7 +106,10 @@ def main(counts_df_path):
 
     control_factors = {}
     for i in int_factors:
-        control_factors[i] = 0
+        if nand20:
+            control_factors[i] = 5
+        else:
+            control_factors[i] = 0
     for bf in bool_factors:
         control_factors[bf] = False
     print("control_factors: {}".format(control_factors))
@@ -75,7 +120,6 @@ def main(counts_df_path):
     counts_df_qcd.reset_index(inplace=True,drop=True)
     print("Unique strains: {}".format(len(counts_df_qcd['Strain'].unique())))
 
-    DE_tests = [['Bacillus subtilis 168 Marburg', 'Bacillus subtilis 168 Marburg']]
     run_dir = os.getcwd() + "/../exp_ref_additive_design"
     if not os.path.exists(run_dir):
         os.makedirs(run_dir)
@@ -124,15 +168,13 @@ def main(counts_df_path):
 
     print("ret_value: {}".format(ret_value))
     if ret_value == 0:
-        file_count = 0
-        while file_count < len(comparison_indices):
-            time.sleep(10)
-            file_count = 0
-            for fname in os.listdir('./results'):
-                if fname.startswith('dge'):
-                    file_count += 1
+        while True:
+            result_files = [f for f in os.listdir('./results') if re.match(r'^(?!dge).*$', f)]
+            if len(result_files) < len(comparison_indices):
+                time.sleep(10)
+            else:
+                break
 
-        print("file_count: {}".format(file_count))
         df_diff_exp = utils.aggregate_dataframes(run_dir, sub_factors, suffix_col='Strain_2')
         dfs=[]
         for test in DE_tests:
@@ -143,7 +185,11 @@ def main(counts_df_path):
             df_sub['strain']=strain_2
             dfs.append(df_sub)
         df_diff_exp_all = pd.concat(dfs)
-        df_diff_additive_design = create_additive_design(df_diff_exp_all)
+        df_diff_additive_design = create_additive_design(df_diff_exp_all, int_cols=bool_factors)
+        print("df_diff_additive_design")
+        print(df_diff_additive_design.head(5))
+        df_diff_additive_design.to_csv(os.path.join(run_dir,'results/additive_design_df.csv'))
         
 if __name__ == '__main__':
-    main("./experiment.ginkgo.29422_ReadCountMatrix_preCAD_transposed_filtered.csv")
+    #main("./experiment.ginkgo.29422_ReadCountMatrix_preCAD_transposed.csv")
+    main("./experiment.ginkgo.23299_ReadCountMatrix_preCAD_transposed.csv")
