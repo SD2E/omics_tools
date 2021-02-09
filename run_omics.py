da81d1ff1eb838387b0eba6a1aa9c5cf88361b02
Mohammed Ali Eslami <meslami@netrias.com>
2021-02-09 16:59:10 -0500

    changed all columns to lowercase and updated config file to reflect change

--- run_omics.py
+++ run_omics.py
@@ -45,7 +45,7 @@ def qc_update(df, factors_to_keep, bool_factors, int_factors):
 
     return df
 
-def create_additive_design(df,int_cols=['IPTG','arabinose']):
+def create_additive_design(df,int_cols=['iptg','arabinose']):
     #genes = set(df.index)
     #genes_index = pd.DataFrame(list(range(len(genes))),columns=['Num_Index'],index=genes)
     #df = df.join(genes_index)
@@ -159,6 +159,7 @@ def main(counts_df_path, config_file, result_dir):
 
     counts_df = pd.read_csv(counts_df_path, sep=',', low_memory=False)
     counts_df.rename({counts_df.columns[0]:'sample_id'},inplace=True,axis=1)
+    counts_df.rename({col: str.lower(col) for col in counts_df.columns},inplace=True, axis=1)
 
     print(counts_df.shape)
 
@@ -166,7 +167,7 @@ def main(counts_df_path, config_file, result_dir):
     int_factors, bool_factors, float_factors, control_factors, cf_value, hrm_experimental_condition_cols, DE_tests, add_one, fdr_max, log_fc_min, batch_delay, output_name, comparison_target_column,base_factor = load_config(config_file)
     print("hrm_experimental_condition_cols: {}".format(hrm_experimental_condition_cols))
     if not base_factor:
-        base_factor = ['Strain']
+        base_factor = ['strain']
     sub_factors = int_factors + bool_factors + float_factors 
     factors_to_keep = base_factor + sub_factors
     print("factors_to_keep: {}".format(factors_to_keep))
@@ -186,9 +187,9 @@ def main(counts_df_path, config_file, result_dir):
     print(counts_df_qcd.head(5))
     counts_df_qcd.reset_index(inplace=True,drop=True)
     try:
-        print("Unique strains: {}".format(len(counts_df_qcd['Strain'].unique())))
+        print("Unique strains: {}".format(len(counts_df_qcd['strain'].unique())))
     except:
-        print('No column named Strain')
+        print('No column named strain')
 
     run_dir = os.path.join(os.getcwd(), result_dir)
     if not os.path.exists(run_dir):
